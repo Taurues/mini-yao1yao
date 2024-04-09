d7f98afdf2da0a221bed8ad423784516a7a873d2
zhangyanru <zhangyanru@wshifu.com>
2024-04-09 10:19:22 +0800

    feat: 打包

--- src/app.config.ts
+++ src/app.config.ts
@@ -19,6 +19,7 @@ export default defineAppConfig({
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
   },
+  entryPagePath: 'pages/index/index',
   tabBar: {
     list: [
       {
